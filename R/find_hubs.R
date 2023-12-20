9c3e68854480ee30bae0e2b66ec318ae4fca6c46
gzahn <gzahn@uvu.edu>
2023-12-20 10:23:00 -0700

    add taxonomy to find_hubs.R

--- R/find_hubs.R
+++ R/find_hubs.R
@@ -3,23 +3,25 @@
 #' Returns a named vector of hub taxa (vertices)
 #'
 #' @import igraph
+#' @import SpiecEasi
 #'
 #' @param graph An igraph object
+#' @param physeq A phyloseq object (must include taxonomy table) that matches the taxa in the igraph object
 #' @param cutoff Method to use for determining hub taxa authority cutoff. One of "midpoint", "median","1sd","2sd
 #' @param layout Which layout to use for vertices. Currently accepts one of: "auto","gem","davidson"
 #' @param littlepoint Base size for non-hub taxa in the plot
 #'
-#' @return plot of igraph network with vertices sized by authority score
+#' @return Data Frame of hub taxa identities and hub scores. colNames = c("hub_taxon","hub_score","vertex_id). If the igraph object does not have vertex names, a vector of hub values for taxa over the threshold is returned instead.
 #'
 #' @examples
-#' plot_hubs(igraph_object)
+#' find_hubs(igraph_object,physeq)
 #'
 #' @export
 
 
 # graph <- readRDS("../Cheeke_Proposal/Output/ITS_igraph_4_out.RDS")
 
-find_hubs <- function(graph,cutoff="median"){
+find_hubs <- function(graph,physeq,cutoff="median"){
 
   a.score <- igraph::authority_score(graph)$vector
   h.score <- igraph::hub_score(graph)$vector
@@ -40,5 +42,36 @@ find_hubs <- function(graph,cutoff="median"){
   hub.taxa <- h.score[h.score > h.cutoff]
   hub.taxa.names <- names(hub.taxa)
 
-  return(hub.taxa)
+  if(length(unique(hub.taxa.names)) == 1){
+    warning("igraph object did not have vertex names. returning unnamed hub values.")
+
+    return(hub.taxa)
+  } else {
+    hub.taxa.indices <- as.numeric(names(hub.taxa))
+    hub.taxonomy <- paste(
+      tax_table(physeq)[hub.taxa.indices,1],
+      tax_table(physeq)[hub.taxa.indices,2],
+      tax_table(physeq)[hub.taxa.indices,3],
+      tax_table(physeq)[hub.taxa.indices,4],
+      tax_table(physeq)[hub.taxa.indices,5],
+      tax_table(physeq)[hub.taxa.indices,6],
+      tax_table(physeq)[hub.taxa.indices,7],
+      sep = ";"
+    )
+
+    vertex.labels <- names(hub.taxa)
+
+    names(hub.taxa) <- hub.taxonomy
+
+    hub.taxa.df <-
+    data.frame(hub_taxon = names(hub.taxa),
+               hub_score = unname(hub.taxa),
+               vertex_id = vertex.labels)
+
+    return(hub.taxa.df)
+  }
+
+
+
+
 }
